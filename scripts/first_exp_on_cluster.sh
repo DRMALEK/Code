16336a8e567236e558d2aff1bd9e262efd0dcf2c
malek <malek97ai@gmail.com>
2025-02-26 16:14:48 +0100

    small change

--- scripts/first_exp_on_cluster.sh
+++ scripts/first_exp_on_cluster.sh
@@ -1,15 +1,19 @@
 #!bin/sh
 
-# define some variables
-export DATA_PATH=/home/st/st_us-053030/st_st181130/MeccanoDataset/RGB_frames
+# Define some variables
+export DATA_PATH=$HOME/MeccanoDataset/RGB_frames
 export WORKSPACE_PATH=$(ws_find first_exp)
 export CODE_PATH=$HOME/code
 
 # first copy the data from the home directory to the scratch directory or temporary directory
 cp -r $DATA_PATH $TMPDIR 
 
+echo "Data is copied to the temporary directory"
+
 # second copy the code from the home directory to the scratch directory or temporary directory
 cp -r $CODE_PATH $WORKSPACE_PATH
+
+echo "Code is copied to the workspace directory"
 
 # third run the code
 
@@ -19,6 +23,8 @@ python3 -m venv $WORKSPACE_PATH/venv
 ## install dependencies
 source $WORKSPACE_PATH/venv/bin/activate
 pip install -r $WORKSPACE_PATH/code/requirements.txt
+
+echo "Dependencies are installed"
 
 ## run the code with specific parameters
 python $WORKSPACE_PATH/code/main.py --training_type train --config $WORKSPACE_PATH/code/config/train/config.yaml
